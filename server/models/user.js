eaee3a1f72057ef8a8299bc6427305a138c2c8da
Ivan Babic <ibabic@fesb.com>
2018-12-03 09:23:17 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -59,13 +59,34 @@ UserSchema.methods.generateAuthToken = function () {
     try {
       decoded = jwt.verify(token, 'abc123');
     } catch (e) {
-        return Promise.reject();
+      return Promise.reject();
     }
   
     return User.findOne({
       '_id': decoded._id,
       'tokens.token': token,
       'tokens.access': 'auth'
+    });
+  };
+
+  UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+  
+    return User.findOne({email}).then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+  
+      return new Promise((resolve, reject) => {
+        // Use bcrypt.compare to compare password and user.password
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
     });
   };
 
